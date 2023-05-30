05f2c392eb7870cd54f0a92b37987309ceef0e67
mazab99 <mazab15499@gmail.com>
2023-05-30 07:34:42 +0300

    finish

---
+++ lib/flutter_3d_viewer.dart
@@ -0,0 +1,10 @@
+
+library flutter_3d_viewer;
+
+export 'src/widget.dart';
+export 'src/scene.dart';
+export 'src/object.dart';
+export 'src/camera.dart';
+export 'src/mesh.dart';
+export 'src/material.dart';
+export 'package:vector_math/vector_math_64.dart' hide Colors;
